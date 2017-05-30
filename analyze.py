4586625f45a3f25a5cdcc0056dd5db1211b05a41
jrcastle <james.castle@ku.edu>
2017-05-30 16:19:01 -0500

    Add new block that determines average rating for a Genre for each occupation

--- analyze.py
+++ analyze.py
@@ -1,12 +1,22 @@
 import math
+import os
 import scipy as sp
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pylab
 
-# Pandas dataframes for the three .dat files
+# Flags on what to analyze
+block0 = True  # Explore the distribution of ratings for different occupations
+block1 = True  # Explore the distribution of ratings for different occupations
+block2 = True  # Explore the average rating for a genre for all occupations
 
+# Clean the plots directory
+print "Cleaning the plots directory..."
+command = "rm plots/*.pdf"
+os.system(command)
+
+# Pandas dataframes for the three .dat files
 print "Loading movies.dat ..."
 dfMovies = pd.read_table("movies.dat", 
                    sep='::', 
@@ -70,72 +80,111 @@ ageGroup[45] = "45-49"
 ageGroup[50] = "50-55"
 ageGroup[56] = "56+"
 
+# List containing all possible genres in the dataset
+genres = [
+    "Action",
+    "Adventure",
+    "Animation",
+    "Children\'s",
+    "Comedy",
+    "Crime",
+    "Documentary",
+    "Drama",
+    "Fantasy",
+    "Film-Noir",
+    "Horror",
+    "Musical",
+    "Mystery",
+    "Romance",
+    "Sci-Fi",
+    "Thriller",
+    "War",
+    "Western"]
+
 # Explore the distribution of occupations who responded #########################################
-occupation_list = dfUsers["Occupation"].tolist()
-bin_boundaries = [i+0.5 for i in range(-1,21)]
+if block0:
+    occupation_list = dfUsers["Occupation"].tolist()
+    bin_boundaries = [i+0.5 for i in range(-1,21)]
 
-fig = plt.hist(occupation_list, bins=bin_boundaries) 
-ax = plt.subplot(111)
-
-# x axis
-ax.set_xlabel("Occupation")
-ax.set_xlim(-1,21)
-x =  [i for i in range(0,21)]
-xl = [occupations[i] for i in range(0,21)]
-plt.xticks(x, xl, rotation='vertical')
-plt.tick_params(axis='x', which='major', labelsize=9)
-plt.gcf().subplots_adjust(bottom=0.35)
-
-# y axis
-ax.set_ylabel("Count")
-
-# save plot
-plt.savefig('plots/OccupationDist.pdf')
-plt.close()
-# Explore the distribution of ratings for different occupations #################################
-bin_boundaries = [i+0.5 for i in range(-1,6)]
-for occ in occupations:
-    occupation_ratings = dfFinal.loc[dfFinal["Occupation"] == occ, "Rating"].tolist()
-    fig = plt.hist(occupation_ratings, bins=bin_boundaries), 
+    fig = plt.hist(occupation_list, bins=bin_boundaries) 
     ax = plt.subplot(111)
 
     # x axis
-    ax.set_xlabel("Rating")
-    ax.set_xlim(0,6)
+    ax.set_xlabel("Occupation")
+    ax.set_xlim(-1,21)
+    x =  [i for i in range(0,21)]
+    xl = [occupations[i] for i in range(0,21)]
+    plt.xticks(x, xl, rotation='vertical')
+    plt.tick_params(axis='x', which='major', labelsize=9)
+    plt.gcf().subplots_adjust(bottom=0.35)
+
     # y axis
     ax.set_ylabel("Count")
-    # title
-    name = occupations[occ] + " Distribution of Ratings"
-    plt.title(name)
+
     # save plot
-    name = 'plots/%sRatings.pdf' % (occupations[occ].replace("/", "_"))
-    name = name.replace(" ", "")
-    print name + " created!"
-    plt.savefig(name)
+    plt.savefig('plots/OccupationDist.pdf')
     plt.close()
-#ENDFOR
+#ENDIF
 
+# Explore the distribution of ratings for different occupations #################################
+if block1:
+    bin_boundaries = [i+0.5 for i in range(-1,6)]
+    for occ in occupations:
+        occupation_ratings = dfFinal.loc[dfFinal["Occupation"] == occ, "Rating"].tolist()
+        fig = plt.hist(occupation_ratings, bins=bin_boundaries), 
+        ax = plt.subplot(111)
 
-#bin_boundaries = [i+0.5 for i in range(-1,6)]
-#for occ in occupations:
-#
-#    if occ not in occupation_ratings:
-#        continue
-#    #ENDIF
-#    fig, ax = plt.subplots()
-#    plt.hist(occupation_ratings[occ], bins=bin_boundaries)
-#
-#    # x axis
-#    ax.set_xlabel("Rating")
-#    ax.set_xlim(0,6)
-#    # y axis
-#    ax.set_ylabel("Count")
-#    # title
-#    name = occupations[occ] + " Distribution of Ratings"
-#    plt.title(name)
-#    # save plot
-#    name = 'plots/%sRatings.pdf' % (occupations[occ].replace("/", "_"))
-#    name = name.replace(" ", "")
-#    print name + " created!"
-#    plt.savefig(name)
-##ENDFOR
+        # x axis
+        ax.set_xlabel("Rating")
+        ax.set_xlim(0,6)
+        # y axis
+        ax.set_ylabel("Count")
+        # title
+        name = occupations[occ] + " Distribution of Ratings"
+        plt.title(name)
+        # save plot
+        name = 'plots/%sRatings.pdf' % (occupations[occ].replace("/", "_"))
+        name = name.replace(" ", "")
+        print name + " created!"
+        plt.savefig(name)
+        plt.close()
+    #ENDFOR
+#ENDIF
+
+# Explore the average rating for a genre for all occupations #################################
+if block2:
+    for gen in genres:
+        x = []
+        y = []
+        for occ in occupations:
+            print "Finding " + gen + "genre average rating for " + occupations[occ] + " occupation..." 
+            occupation_ratings = dfFinal.loc[ (dfFinal["Occupation"] == occ) & (dfFinal["Genres"].str.contains(gen)), "Rating"].tolist()
+            ave_rating = sum(occupation_ratings) / len(occupation_ratings)
+            x.append(occ)
+            y.append(ave_rating)
+            #print "Average Rating = " + str(ave_rating)
+        #ENDFOR
+        fig = plt.bar(x, y, 1),
+        ax = plt.subplot(111)
+        # x axis
+        ax.set_xlabel("Occupation")
+        ax.set_xlim(-1,21)
+        x =  [i for i in range(0,21)]
+        xl = [occupations[i] for i in range(0,21)]
+        plt.xticks(x, xl, rotation='vertical')
+        plt.tick_params(axis='x', which='major', labelsize=9)
+        plt.gcf().subplots_adjust(bottom=0.35)
+        # y axis
+        ax.set_ylabel("Average Rating")
+        ax.set_ylim(0,5)
+        # title
+        name = gen + " Genre: Average Rating Per Occupation"
+        plt.title(name)
+        # save plot
+        name = 'plots/Genre%s_AveRating_VS_occupation.pdf' % gen.replace("\\'", "")
+        name = name.replace(" ", "")
+        print name + " created!"
+        plt.savefig(name)
+        plt.close()
+    #ENDFOR
+#ENDIF
