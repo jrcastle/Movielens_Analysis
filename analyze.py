3c8208d6005d2f9b02cc143abb1ba4ec321a811f
jrcastle <james.castle@ku.edu>
2017-06-01 10:13:51 -0500

    Add new block to calculate average rating for age group. Added TODO list

--- analyze.py
+++ analyze.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pylab
 
 # Flags on what to analyze
-block0 = 1  # Explore the distribution of ratings for different occupations
+block0 = 1  # Explore the distribution of occupations who responded
 block1 = 1  # Explore the distribution of ratings for different occupations
 block2 = 1  # Explore the average rating for a genre for all occupations
 
@@ -17,34 +17,43 @@ block2 = 1  # Explore the average rating for a genre for all occupations
 #os.system(command)
 
 # Pandas dataframes for the three .dat files
-print "Loading movies.dat ..."
-dfMovies = pd.read_table("movies.dat", 
-                   sep='::', 
-                   engine="python", 
-                   header=None
-                   )
-dfMovies.columns = ['MovieID', 'Title', 'Genres']
+if not os.path.isfile( "MergedDataset.csv" ):
+    print "Loading movies.dat ..."
+    dfMovies = pd.read_table("movies.dat", 
+                             sep='::', 
+                             engine="python", 
+                             header=None
+                             )
+    dfMovies.columns = ['MovieID', 'Title', 'Genres']
 
-print "Loading ratings.dat ..."
-dfRatings = pd.read_table("ratings.dat",
-                          sep='::',
-                          engine="python",
-                          header=None
+    print "Loading ratings.dat ..."
+    dfRatings = pd.read_table("ratings.dat",
+                              sep='::',
+                              engine="python",
+                              header=None
+                              )
+    dfRatings.columns = ['UserID', 'MovieID', 'Rating', 'Timestamp']
+
+    print "Loading users.dat ..."
+    dfUsers = pd.read_table("users.dat",
+                            sep='::',
+                            engine="python",
+                            header=None
+                            )
+    dfUsers.columns = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code']
+
+    # Merge dataframes
+    print "Merging dataframes ..."
+    dfMerged = pd.merge(dfRatings, dfUsers, how = 'outer')
+    dfFinal  = pd.merge(dfMerged, dfMovies, how = 'outer')
+    del dfMerged
+
+    dfFinal.to_csv("MergedDataset.csv")
+else:
+    print "Loading MergedDataset.csv ..."
+    dfFinal = pd.read_csv("MergedDataset.csv",
+                          engine="python"
                           )
-dfRatings.columns = ['UserID', 'MovieID', 'Rating', 'Timestamp']
-
-print "Loading users.dat ..."
-dfUsers = pd.read_table("users.dat",
-                        sep='::',
-                        engine="python",
-                        header=None
-                        )
-dfUsers.columns = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code']
-
-# Merge dataframes
-dfMerged = pd.merge(dfRatings, dfUsers, how = 'outer')
-dfFinal  = pd.merge(dfMerged, dfMovies, how = 'outer')
-del dfMerged
 
 # Dictionary that maps occupation code to occupation
 occupations = {}
@@ -103,6 +112,13 @@ genres = [
 
 # Explore the distribution of occupations who responded #########################################
 if block0:
+    dfUsers = pd.read_table("users.dat",
+                            sep='::',
+                            engine="python",
+                            header=None
+                            )
+    dfUsers.columns = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code']
+
     occupation_list = dfUsers["Occupation"].tolist()
     bin_boundaries = [i+0.5 for i in range(-1,21)]
 
@@ -124,6 +140,8 @@ if block0:
     # save plot
     plt.savefig('plots/OccupationDist.pdf')
     plt.close()
+
+    del dfUsers
 #ENDIF
 
 # Explore the distribution of ratings for different occupations #################################
@@ -143,7 +161,7 @@ if block1:
         name = occupations[occ] + " Distribution of Ratings"
         plt.title(name)
         # save plot
-        name = 'plots/%sRatings.pdf' % (occupations[occ].replace("/", "_"))
+        name = 'plots/OccupationDistnOfRatings/%sRatings.pdf' % (occupations[occ].replace("/", "_"))
         name = name.replace(" ", "")
         print name + " created!"
         plt.savefig(name)
@@ -151,19 +169,20 @@ if block1:
     #ENDFOR
 #ENDIF
 
-# Explore the average rating for a genre for all occupations #################################
+# Explore the average rating for a genre for all occupations/ages #################################
 if block2:
     for gen in genres:
-        x  = []
+
+        # Occupation
+        x =  [i for i in range(0,len(occupations))]
+        xl = [occupations[i] for i in range(0,len(occupations))]
         y  = []
         ye = []
         for occ in occupations:
-            print "Finding " + gen + " genre average rating for " + occupations[occ] + " occupation..." 
             occupation_ratings = dfFinal.loc[ (dfFinal["Occupation"] == occ) & (dfFinal["Genres"].str.contains(gen)), "Rating"].tolist()
             occupation_ratings = np.array(occupation_ratings)
             ave_rating = np.average( occupation_ratings )
             std_rating = np.std( occupation_ratings )
-            x.append(occ)
             y.append(ave_rating)
             ye.append(std_rating)
             #print "Average Rating = " + str(ave_rating)
@@ -174,9 +193,7 @@ if block2:
 
         # x axis
         ax.set_xlabel("Occupation")
-        ax.set_xlim(-1,21)
-        x =  [i for i in range(0,21)]
-        xl = [occupations[i] for i in range(0,21)]
+        ax.set_xlim(-1,len(occupations))
         plt.xticks(x, xl, rotation='vertical')
         plt.tick_params(axis='x', which='major', labelsize=9)
         plt.gcf().subplots_adjust(bottom=0.35)
@@ -190,10 +207,58 @@ if block2:
         plt.title(name)
 
         # save plot
-        name = 'plots/Genre%s_AveRating_VS_occupation.pdf' % gen.replace("\\'", "")
+        name = 'plots/GenreAveRatingVSOccupation/Genre%s_AveRating_VS_occupation.pdf' % gen.replace("\\'", "")
         name = name.replace(" ", "")
         print name + " created!"
         plt.savefig(name)
         plt.close()
+
+        # Age group
+        x =  [i for i in range(0,len(ageGroup))]
+        xl = [ageGroup[i] for i in sorted(ageGroup.keys())]
+        y  = []
+        ye = []
+        for age in ageGroup:
+            age_ratings = dfFinal.loc[ (dfFinal["Age"] == age) & (dfFinal["Genres"].str.contains(gen)), "Rating"].tolist()
+            age_ratings = np.array( age_ratings )
+            ave_rating = np.average( age_ratings )
+            std_rating = np.std( age_ratings )
+            y.append(ave_rating)
+            ye.append(std_rating)
+        #ENDFOR
+
+        fig = plt.bar(x, y, 1, yerr = ye),
+        ax = plt.subplot(111)
+
+        # x axis
+        ax.set_xlabel("Age Group")
+        ax.set_xlim(-1,len(ageGroup))
+        plt.xticks(x, xl)#, rotation='vertical')
+        plt.tick_params(axis='x', which='major', labelsize=9)
+        plt.gcf().subplots_adjust(bottom=0.15)
+
+        # y axis
+        ax.set_ylabel("Average Rating")
+        ax.set_ylim(0,5)
+
+        # title
+        name = gen + " Genre: Average Rating Per Age Group"
+        plt.title(name)
+
+        # save plot
+        name = 'plots/GenreAveRatingVSAge/Genre%s_AveRating_VS_Age.pdf' % gen.replace("\\'", "")
+        name = name.replace(" ", "")
+        print name + " created!"
+        plt.savefig(name)
+        plt.close()
+
     #ENDFOR
 #ENDIF
+
+#################################  TODO #################################
+# Remove the apostrophy in Children's when saved to pdf
+# Try to speed up the average rating calculations.  Maybe try making a smaller dataframe and then slicing that? 
+#
+# Explore average rating for a genre for each gender
+#   * two color plot Ave rating VS genre where each color is a gender
+
